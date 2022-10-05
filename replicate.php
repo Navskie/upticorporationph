075ceaaf72fcf39525ee6557c7531eedf7915758
Navskie <85003094+Navskie@users.noreply.github.com>
2022-10-05 17:26:37 +0800

    test replicate

--- replicate.php
+++ replicate.php
@@ -1,1 +1,5 @@
-replicate <?php echo $_GET['id'] ?>
+replicate <?php echo $id = $_GET['id'] ?>
+
+<form action="replicate-process.php?id=<?php echo $id ?>" method="post">
+  <button>Shop Now</button>
+</form>
