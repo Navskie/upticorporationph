aac40eb2f906d66f48798098a07f9014dde45bf6
SR PROGRAMMER <uptimisedcorporationph@gmail.com>
2022-11-05 14:48:30 +0800

    test

--- system/dbms/conn.php
+++ system/dbms/conn.php
@@ -1,6 +1,6 @@
 <?php
     
-    // $connect = mysqli_connect('localhost', 'u438882109_betatestph', '@User2022', 'u438882109_betatestph'); 
-    $connect = mysqli_connect('localhost', 'root', '', 'uptimisedph');
+    $connect = mysqli_connect('localhost', 'u438882109_betatestph', '@User2022', 'u438882109_betatestph'); 
+    // $connect = mysqli_connect('localhost', 'root', '', 'uptimisedph');
 
 ?>  
